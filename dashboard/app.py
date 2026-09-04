a68c39c5cd3b4c3e997540da325df9bbc45e580d
adubale <aarondubale@gmail.com>
2026-09-04 00:06:39 -0700

    feat: core performance metrics visible in dashboard for VM

--- dashboard/app.py
+++ dashboard/app.py
@@ -1,5 +1,7 @@
 from flask import Flask, render_template
 import docker
+import psutil
+import time
 
 app = Flask(__name__)
 
@@ -9,9 +11,17 @@ client = docker.from_env()
 def home():
     containers = client.containers.list(all=True)
 
+    system = {
+            "cpu": psutil.cpu_percent(interval=0.5),
+            "memory": psutil.virtual_memory().percent,
+            "disk": psutil.disk_usage("/").percent,
+            "uptime": int(time.time() - psutil.boot_time())
+    }
+
     return render_template(
             "index.html",
-            containers=containers
+            containers=containers,
+            system=system
             )
 
 if __name__ == "__main__":
