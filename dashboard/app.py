36b20a6e441f3656dc496fd71cbd223ac7ca5a93
adubale <aarondubale@gmail.com>
2026-09-04 00:02:12 -0700

    feat: docker containers and statuses visible in dashboard

--- dashboard/app.py
+++ dashboard/app.py
@@ -1,10 +1,18 @@
 from flask import Flask, render_template
+import docker
 
 app = Flask(__name__)
 
+client = docker.from_env()
+
 @app.route("/")
 def home():
-    return render_template("index.html")
+    containers = client.containers.list(all=True)
+
+    return render_template(
+            "index.html",
+            containers=containers
+            )
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000, debug=True)
